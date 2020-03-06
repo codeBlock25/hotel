68e863111a574401883d5cce702f1c34b49ab8ad
classic-code <50813314+codeBlock25@users.noreply.github.com>
2020-03-06 15:41:57 +0100

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <section>
+    <Navigation />
+    <Home />
+  </section>
+</template>
+
+<script>
+import './index.css'
+import Home from "./view/home"
+import Navigation from "./components/nav-bar"
+
+export default {
+  name: 'App',
+  components: {
+    Home,
+    Navigation,
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/view/home.vue
@@ -0,0 +1,409 @@
+<template>
+  <section class="Home">
+    <div class="first">
+      <div class="cover">
+        <div class="contents">
+          <h3>WE deliver comfort. adipisicing elit. Accusamus tempo</h3>
+          <div class="casts">
+            <div class="cast"></div>
+            <div class="cast"></div>
+            <div class="cast"></div>
+            <div class="cast"></div>
+          </div>
+        </div>
+        <div class="booking"></div>
+      </div>
+    </div>
+    <div class="second">
+      <h3 class="title">compilation <span>view all</span></h3>
+      <Observer
+        v-bind:threshold="[0.3]"
+        @on-change="observerMain"
+        v-bind:class="observered ? 'container showing' : 'container'"
+      >
+        <Observer class="rooms">
+          <div
+            class="gal"
+            style="background-image: url('/images/pic5.jpg');"
+          ></div>
+          <div class="desc">
+            <h3>lectury 2 rooms full sitter</h3>
+            <p><span>&#8358;30,000</span> per night</p>
+            <span class="like">
+              <svg
+                version="1.1"
+                id="Layer_1"
+                xmlns="http://www.w3.org/2000/svg"
+                xmlns:xlink="http://www.w3.org/1999/xlink"
+                x="0px"
+                y="0px"
+                viewBox="0 0 511.269 511.269"
+                style="enable-background:new 0 0 511.269 511.269;"
+                xml:space="preserve"
+              >
+                <path
+                  style="fill:#F3705A;"
+                  d="M140.367,465.067C116.9,438.4,93.434,410.667,78.5,377.6c-14.933-35.2-19.2-75.733-11.733-114.133
+                  s24.533-74.667,49.067-105.6c-2.133,26.667,7.467,54.4,25.6,74.667c-10.667-51.2,6.4-106.667,40.533-147.2S263.034,18.133,312.1,0
+                  c-24.533,25.6-27.733,66.133-18.133,100.267c9.6,34.133,29.867,64,48,94.933c18.133,30.933,35.2,62.933,36.267,98.133
+                  c9.6-18.133,20.267-36.267,26.667-56.533c6.4-20.267,9.6-41.6,4.267-61.867c19.2,23.467,29.867,46.933,35.2,76.8
+                  c5.333,29.867,4.267,60.8,1.067,90.667c-4.267,33.067-12.8,67.2-30.933,94.933c-21.333,33.067-55.467,56.533-92.8,69.333
+                  C255.567,518.4,190.5,508.8,140.367,465.067z"
+                />
+                <path
+                  style="fill:#FFD15C;"
+                  d="M221.434,504.533C308.9,538.667,395.3,435.2,347.3,355.2c0-1.067-1.067-1.067-1.067-2.133
+                  c4.267,43.733-6.4,75.733-26.667,93.867c10.667-25.6,3.2-55.467-9.6-81.067c-12.8-24.533-30.933-46.933-44.8-70.4
+                  c-13.867-24.533-24.533-52.267-18.133-80c-25.6,19.2-43.733,48-51.2,78.933c-7.467,30.933-3.2,65.067,10.667,93.867
+                  c-16-11.733-27.733-30.933-28.8-51.2c-17.067,20.267-27.733,46.933-26.667,73.6C151.034,452.267,184.1,489.6,221.434,504.533z"
+                />
+              </svg>
+            </span>
+          </div>
+        </Observer>
+        <div class="rooms">
+          <div
+            class="gal"
+            style="background-image: url('/images/pic2.jpg');"
+          ></div>
+          <div class="desc">
+            <h3>lectury 2 rooms full sitter</h3>
+            <p><span>&#8358;30,000</span> per night</p>
+            <span class="like"
+              ><svg
+                xmlns="http://www.w3.org/2000/svg"
+                xmlns:xlink="http://www.w3.org/1999/xlink"
+                version="1.1"
+                id="Capa_1"
+                x="0px"
+                y="0px"
+                viewBox="0 0 22.778 22.778"
+                style="enable-background:new 0 0 22.778 22.778;"
+                xml:space="preserve"
+                class="boom"
+              >
+                <g>
+                  <g>
+                    <g>
+                      <polygon
+                        points="17.931,15.433 18.83,10.328 14.412,12.625 12.438,8.625 10.468,12.616 6.05,10.269     6.912,15.437 2.098,16.137 4.685,18.66 5.68,18.66 3.59,16.622 7.713,16.021 6.967,11.542 10.773,13.566 12.438,10.193     14.103,13.568 17.903,11.593 17.122,16.017 21.285,16.622 19.196,18.66 20.19,18.66 22.778,16.137   "
+                        data-original="#030104"
+                        class="hovered-path active-path"
+                        data-old_color="#030104"
+                        fill="#FF5D5D"
+                      />
+                      <polygon
+                        points="16.322,16.545 16.95,12.977 13.84,14.594 12.427,11.732 11.02,14.583 7.917,12.935     8.518,16.548 5.119,17.042 6.777,18.66 8.766,18.66 8.102,18.012 10.121,17.718 9.748,15.481 11.631,16.483 12.427,14.87     13.222,16.479 15.095,15.507 14.704,17.714 16.752,18.012 16.088,18.66 18.076,18.66 19.737,17.042   "
+                        data-original="#030104"
+                        class="hovered-path active-path"
+                        data-old_color="#030104"
+                        fill="#FF5D5D"
+                      />
+                      <path
+                        d="M12.438,17.909c-0.503,0-0.91,0.408-0.91,0.911h1.822C13.35,18.316,12.942,17.909,12.438,17.909z"
+                        data-original="#030104"
+                        class="hovered-path active-path"
+                        data-old_color="#030104"
+                        fill="#FF5D5D"
+                      />
+                      <polygon
+                        points="17.915,3.958 14.888,10.515 15.382,10.72   "
+                        data-original="#030104"
+                        class="hovered-path active-path"
+                        data-old_color="#030104"
+                        fill="#FF5D5D"
+                      />
+                      <polygon
+                        points="5.694,14.399 0,9.958 5.348,14.809   "
+                        data-original="#030104"
+                        class="hovered-path active-path"
+                        data-old_color="#030104"
+                        fill="#FF5D5D"
+                      />
+                      <polygon
+                        points="21.633,11.866 19.072,14.083 19.453,14.458   "
+                        data-original="#030104"
+                        class="hovered-path active-path"
+                        data-old_color="#030104"
+                        fill="#FF5D5D"
+                      />
+                      <polygon
+                        points="10.303,10.5 8.866,7.43 9.801,10.688   "
+                        data-original="#030104"
+                        class="hovered-path active-path"
+                        data-old_color="#030104"
+                        fill="#FF5D5D"
+                      />
+                    </g>
+                  </g>
+                </g></svg
+            ></span>
+          </div>
+        </div>
+        <div class="rooms">
+          <div
+            class="gal"
+            style="background-image: url('/images/pic6.jpg');"
+          ></div>
+          <div class="desc">
+            <h3>lectury 2 rooms full sitter</h3>
+            <p><span>&#8358;30,000</span> per night</p>
+            <span class="like">
+              <svg
+                version="1.1"
+                id="Layer_1"
+                xmlns="http://www.w3.org/2000/svg"
+                xmlns:xlink="http://www.w3.org/1999/xlink"
+                x="0px"
+                y="0px"
+                viewBox="0 0 511.269 511.269"
+                style="enable-background:new 0 0 511.269 511.269;"
+                xml:space="preserve"
+              >
+                <path
+                  style="fill:#F3705A;"
+                  d="M140.367,465.067C116.9,438.4,93.434,410.667,78.5,377.6c-14.933-35.2-19.2-75.733-11.733-114.133
+                  s24.533-74.667,49.067-105.6c-2.133,26.667,7.467,54.4,25.6,74.667c-10.667-51.2,6.4-106.667,40.533-147.2S263.034,18.133,312.1,0
+                  c-24.533,25.6-27.733,66.133-18.133,100.267c9.6,34.133,29.867,64,48,94.933c18.133,30.933,35.2,62.933,36.267,98.133
+                  c9.6-18.133,20.267-36.267,26.667-56.533c6.4-20.267,9.6-41.6,4.267-61.867c19.2,23.467,29.867,46.933,35.2,76.8
+                  c5.333,29.867,4.267,60.8,1.067,90.667c-4.267,33.067-12.8,67.2-30.933,94.933c-21.333,33.067-55.467,56.533-92.8,69.333
+                  C255.567,518.4,190.5,508.8,140.367,465.067z"
+                />
+                <path
+                  style="fill:#FFD15C;"
+                  d="M221.434,504.533C308.9,538.667,395.3,435.2,347.3,355.2c0-1.067-1.067-1.067-1.067-2.133
+                  c4.267,43.733-6.4,75.733-26.667,93.867c10.667-25.6,3.2-55.467-9.6-81.067c-12.8-24.533-30.933-46.933-44.8-70.4
+                  c-13.867-24.533-24.533-52.267-18.133-80c-25.6,19.2-43.733,48-51.2,78.933c-7.467,30.933-3.2,65.067,10.667,93.867
+                  c-16-11.733-27.733-30.933-28.8-51.2c-17.067,20.267-27.733,46.933-26.667,73.6C151.034,452.267,184.1,489.6,221.434,504.533z"
+                />
+              </svg>
+            </span>
+          </div>
+        </div>
+        <div class="rooms">
+          <div
+            class="gal"
+            style="background-image: url('/images/pic3.jpg');"
+          ></div>
+          <div class="desc">
+            <h3>lectury 2 rooms full sitter</h3>
+            <p><span>&#8358;30,000</span> per night</p>
+            <span class="like"
+              ><svg
+                xmlns="http://www.w3.org/2000/svg"
+                xmlns:xlink="http://www.w3.org/1999/xlink"
+                version="1.1"
+                id="Capa_1"
+                x="0px"
+                y="0px"
+                viewBox="0 0 22.778 22.778"
+                style="enable-background:new 0 0 22.778 22.778;"
+                xml:space="preserve"
+                class="boom"
+              >
+                <g>
+                  <g>
+                    <g>
+                      <polygon
+                        points="17.931,15.433 18.83,10.328 14.412,12.625 12.438,8.625 10.468,12.616 6.05,10.269     6.912,15.437 2.098,16.137 4.685,18.66 5.68,18.66 3.59,16.622 7.713,16.021 6.967,11.542 10.773,13.566 12.438,10.193     14.103,13.568 17.903,11.593 17.122,16.017 21.285,16.622 19.196,18.66 20.19,18.66 22.778,16.137   "
+                        data-original="#030104"
+                        class="hovered-path active-path"
+                        data-old_color="#030104"
+                        fill="#FF5D5D"
+                      />
+                      <polygon
+                        points="16.322,16.545 16.95,12.977 13.84,14.594 12.427,11.732 11.02,14.583 7.917,12.935     8.518,16.548 5.119,17.042 6.777,18.66 8.766,18.66 8.102,18.012 10.121,17.718 9.748,15.481 11.631,16.483 12.427,14.87     13.222,16.479 15.095,15.507 14.704,17.714 16.752,18.012 16.088,18.66 18.076,18.66 19.737,17.042   "
+                        data-original="#030104"
+                        class="hovered-path active-path"
+                        data-old_color="#030104"
+                        fill="#FF5D5D"
+                      />
+                      <path
+                        d="M12.438,17.909c-0.503,0-0.91,0.408-0.91,0.911h1.822C13.35,18.316,12.942,17.909,12.438,17.909z"
+                        data-original="#030104"
+                        class="hovered-path active-path"
+                        data-old_color="#030104"
+                        fill="#FF5D5D"
+                      />
+                      <polygon
+                        points="17.915,3.958 14.888,10.515 15.382,10.72   "
+                        data-original="#030104"
+                        class="hovered-path active-path"
+                        data-old_color="#030104"
+                        fill="#FF5D5D"
+                      />
+                      <polygon
+                        points="5.694,14.399 0,9.958 5.348,14.809   "
+                        data-original="#030104"
+                        class="hovered-path active-path"
+                        data-old_color="#030104"
+                        fill="#FF5D5D"
+                      />
+                      <polygon
+                        points="21.633,11.866 19.072,14.083 19.453,14.458   "
+                        data-original="#030104"
+                        class="hovered-path active-path"
+                        data-old_color="#030104"
+                        fill="#FF5D5D"
+                      />
+                      <polygon
+                        points="10.303,10.5 8.866,7.43 9.801,10.688   "
+                        data-original="#030104"
+                        class="hovered-path active-path"
+                        data-old_color="#030104"
+                        fill="#FF5D5D"
+                      />
+                    </g>
+                  </g>
+                </g></svg
+            ></span>
+          </div>
+        </div>
+      </Observer>
+    </div>
+    <div class="third">
+      <h3 class="title">
+        Our Rooms <small>(most booked of 50)</small> <span>view all</span>
+      </h3>
+      <div class="table">
+        <div class="row">
+          <div class="column">Classification</div>
+          <div class="column">Classification</div>
+          <div class="column">Classification</div>
+          <div class="column">Classification</div>
+          <div class="column">Classification</div>
+          <div class="column">Classification</div>
+          <div class="column">Classification</div>
+          <div class="column">Classification</div>
+          <div class="column">Classification</div>
+          <div class="column">Classification</div>
+          <span class="book_btn">Book now</span>
+        </div>
+        <div class="row">
+          <div class="column">Classification</div>
+          <div class="column">Classification</div>
+          <div class="column">Classification</div>
+          <div class="column">Classification</div>
+          <div class="column">Classification</div>
+          <div class="column">Classification</div>
+          <div class="column">Classification</div>
+          <div class="column">Classification</div>
+          <div class="column">Classification</div>
+          <div class="column">Classification</div>
+        </div>
+        <div class="row">
+          <div class="column">Classification</div>
+          <div class="column">Classification</div>
+          <div class="column">Classification</div>
+          <div class="column">Classification</div>
+          <div class="column">Classification</div>
+          <div class="column">Classification</div>
+          <div class="column">Classification</div>
+          <div class="column">Classification</div>
+          <div class="column">Classification</div>
+          <div class="column">Classification</div>
+        </div>
+        <div class="row">
+          <div class="column">Classification</div>
+          <div class="column">Classification</div>
+          <div class="column">Classification</div>
+          <div class="column">Classification</div>
+          <div class="column">Classification</div>
+          <div class="column">Classification</div>
+          <div class="column">Classification</div>
+          <div class="column">Classification</div>
+          <div class="column">Classification</div>
+          <div class="column">Classification</div>
+        </div>
+        <div class="row">
+          <div class="column">Classification</div>
+          <div class="column">Classification</div>
+          <div class="column">Classification</div>
+          <div class="column">Classification</div>
+          <div class="column">Classification</div>
+          <div class="column">Classification</div>
+          <div class="column">Classification</div>
+          <div class="column">Classification</div>
+          <div class="column">Classification</div>
+          <div class="column">Classification</div>
+        </div>
+        <div class="row">
+          <div class="column">Classification</div>
+          <div class="column">Classification</div>
+          <div class="column">Classification</div>
+          <div class="column">Classification</div>
+          <div class="column">Classification</div>
+          <div class="column">Classification</div>
+          <div class="column">Classification</div>
+          <div class="column">Classification</div>
+          <div class="column">Classification</div>
+          <div class="column">Classification</div>
+        </div>
+        <div class="row">
+          <div class="column">Classification</div>
+          <div class="column">Classification</div>
+          <div class="column">Classification</div>
+          <div class="column">Classification</div>
+          <div class="column">Classification</div>
+          <div class="column">Classification</div>
+          <div class="column">Classification</div>
+          <div class="column">Classification</div>
+          <div class="column">Classification</div>
+          <div class="column">Classification</div>
+        </div>
+      </div>
+    </div>
+    <div class="forth">
+      <div
+        class="pic"
+        style="background-image: url(/images/pic1.jpg); background-color: rgb(37, 37, 38); color: white"
+      >
+        <div class="desc">
+          <h4 class="title">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil
+          </h4>
+          <p class="txt">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Distinctio, error dolorum tempore, obcaecati nemo, est nostrum
+            facere dolore laboriosam unde excepturi in quisquam sunt. Ratione
+            eius minima quos facere repellat.
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import Observer from "vue-intersection-observer";
+
+export default {
+  name: "home",
+  components: {
+    Observer
+  },
+  data() {
+    return {
+      observered: false,
+      headerDark: false
+    };
+  },
+  methods: {
+    observer(entry) {
+      if (entry.isIntersecting) {
+        // unobserve()
+      }
+    },
+    observerMain(entry, unobserve) {
+      if (entry.isIntersecting) {
+        this.observered = true;
+        unobserve();
+      }
+    }
+  }
+};
+</script>
+
+<style scoped lang="scss">
+@import "../styles/home.scss";
+</style>
